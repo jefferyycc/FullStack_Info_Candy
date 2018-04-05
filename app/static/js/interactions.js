eefa8d433f0e979ba53f3d540bdf15050cef6903
aliceyang <aliceyang@berkeley.edu>
2018-04-05 10:50:56 -0700

    v1

--- app/static/js/interactions.js
+++ app/static/js/interactions.js
@@ -1,1 +1,9 @@
 // add something if we need some interaction in javascript.
+  // var elem = document.querySelector('select');
+  // var instance = M.FormSelect.init(elem, options);
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('select').formSelect();
+  });
