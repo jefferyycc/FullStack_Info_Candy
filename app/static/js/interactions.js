577b69dd07d2db2898c30b5cde45b89d9447e278
aliceyang <aliceyang@berkeley.edu>
2018-04-10 17:45:02 -0700

    v2

--- app/static/js/interactions.js
+++ app/static/js/interactions.js
@@ -5,5 +5,29 @@
   // Or with jQuery
 
   $(document).ready(function(){
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+
+  setInterval(function(){
+  	$('.carousel.carousel-slider').carousel('next');
+  },2000)
+  });
+
+  $(document).ready(function(){
+    var elem = document.querySelector('.fixed-action-btn');
+  	var instance = M.FloatingActionButton.init(elem, {
+    direction: 'left',
+    hoverEnabled: false
+  });
+  });
+  
+  $(document).ready(function(){
+    $('.parallax').parallax();
+  });
+
+   $(document).ready(function(){
     $('select').formSelect();
   });
